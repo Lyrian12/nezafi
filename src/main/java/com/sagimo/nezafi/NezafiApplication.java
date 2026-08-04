5b68eb7b92d6182ad8ee983e1cc8aae95db2c67b
lyrian12 <lyriannono@gmail.com>
2026-08-04 13:31:19 +0100

    first commit for initializing the project

---
+++ src/main/java/com/sagimo/nezafi/NezafiApplication.java
@@ -0,0 +1,13 @@
+package com.sagimo.nezafi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NezafiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NezafiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagimo/nezafi/NezafiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagimo.nezafi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NezafiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
